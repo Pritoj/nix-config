da55718bc83a762f2c72884623805aee067cdc76
Pritoj Singh <pritojs@gmail.com>
2026-06-23 15:33:36 +0000

    feat: git configuration

---
+++ nix/modules/git.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = { 
+    enable = true;
+    settings = {
+      user.name = "Pritoj Singh";
+      user.email = "pritojs@gmail.com";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+    };
+  };
+}
