14f011079245ad174995c66be61475033ea4c40a
Pritoj <pritojs@gmail.com>
2026-06-24 22:30:32 +0200

    feat(mac): Add macOS Home Manager configuration

---
+++ nix/home-mac.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./common.nix
+  ];
+
+  home = {
+    username = "pritojs";
+    homeDirectory = "/Users/pritojs";
+    stateVersion = "26.05";
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Pritoj Singh";
+      user.email = "pritojs@gmail.com";
+    };
+  };
+}
