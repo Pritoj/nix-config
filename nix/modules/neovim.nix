4e19d0af0cd9f8568c94a4e9938dbd1aa1958e32
Pritoj Singh <pritojs@gmail.com>
2026-06-24 10:13:49 +0000

    feat(neovim): lsp servers

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -23,6 +23,20 @@
       settings.indent.enable = true;
     };
 
+    plugins.lspconfig.enable = true;
+
+    lsp.servers = {
+      lua_ls.enable = true;
+      tsgo.enable = true;
+      bashls.enable = true;
+      jsonls.enable = true;
+      yamlls.enable = true;
+      cssls.enable = true;
+      html.enable = true;
+      rust_analyzer.enable = true;
+      nixd.enable = true;
+    };
+
     opts = {
       expandtab = true;
       shiftwidth = 2;
