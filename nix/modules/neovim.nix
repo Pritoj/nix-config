7c27897d374460ca2527300d51056c943cb9b8b2
Pritoj Singh <pritojs@gmail.com>
2026-06-24 09:57:54 +0000

    feat: colorscheme and some basic nvim upgrades

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -5,6 +5,29 @@
     enable = true;
     defaultEditor = true;
     nixpkgs.pkgs = pkgs;
+
+    colorschemes.tokyonight = {
+      enable = true;
+      settings.style = "night";
+    };
+
+    opts = {
+      expandtab = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      softtabstop = 2;
+      smartindent = true;
+      relativenumber = true;
+      number = true;
+      signcolumn = "yes";
+      clipboard = "unnamedplus";
+      updatetime = 100;
+    };
+
+    globals = {
+      mapleader = " ";
+      maplocalleader = "\\";
+    };
   };
 
   home.packages = with pkgs; [
