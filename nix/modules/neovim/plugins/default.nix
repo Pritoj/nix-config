eab14d4767c0ae63faafe89bd67e0a6391c07e5a
Pritoj Singh <pritojs@gmail.com>
2026-06-24 11:16:41 +0000

    feat(neovim): More QoL improvements

--- nix/modules/neovim/plugins/default.nix
+++ nix/modules/neovim/plugins/default.nix
@@ -7,4 +7,23 @@
     ./navigation.nix
     ./ufo.nix
   ];
+
+  programs.nixvim.plugins = {
+    barbecue.enable = true;
+    fastaction.enable = true;
+    markview.enable = true;
+    nvim-autopairs.enable = true;
+    lazydev.enable = true;
+    navbuddy = {
+      enable = true;
+      settings.lsp.auto_attach = true;
+    };
+    mini = {
+      enable = true;
+      modules = {
+        surround = { };
+        ai = { };
+      };
+    };
+  };
 }
